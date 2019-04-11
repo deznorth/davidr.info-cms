42b0decdd91b19d0eb73aa9d5b9b11d92ebcbdfa
David Rojas <deznorth@gmail.com>
2019-04-11 17:14:25 -0400

    fixed cors leftovers, app.listen and dev const

--- api/app.js
+++ api/app.js
@@ -14,13 +14,13 @@ const sitemetaRoutes = require('./routes/sitemeta');
 const projectRoutes = require('./routes/project');
 
 //Development environment setup [if in dev, you'll need a valid svars.json file]
-const dev = app.get('env') !== 'production';
+const dev = process.env.NODE_ENV !== 'production';
 const svarsFile = dev ? require('./config/svars.json') : {};
 const PORT = dev ? 5000 : process.env.PORT;
 
 //App Setup
-app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(path.resolve('build')));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.json());
 app.use(methodOverride("_method"));
 
@@ -33,7 +33,6 @@ if(!dev){
 
 if(dev){
     app.use(morgan('dev'));
-    app.use(cors());
 }
 
 //Database Setup
@@ -53,7 +52,7 @@ app.get('*', (req,res) => {
 
 app.listen(PORT, err => {
     if (err) console.log(err);
-    if(!dev){
+    else if(!dev){
         console.log('Server successfully started');
     } else {
         console.log('DEV Server started! go to http://localhost:%s',PORT);
